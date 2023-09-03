b4ca002148002b932d719728413682295e76887d
JanikNex <18660622+JanikNex@users.noreply.github.com>
2023-09-03 22:54:20 +0200

    implemented basic instance resolution for instances and macros

--- src/language-server/generator/mml-serializer.ts
+++ src/language-server/generator/mml-serializer.ts
@@ -2,10 +2,14 @@ import {Model} from "../generated/ast";
 import {MmlReferenceStorage} from "./mml-reference-storage";
 import {SerializedModel} from "./mml-entity-templates";
 import {ModelModelingLanguageServices} from "../model-modeling-language-module";
-
+import {SerializedInstances} from "./mml-instance-templates";
+import {MmlInstanceRegistry} from "./mml-instance-registry";
+import {jsonReplacer} from "./utils";
 
 export function serializeModel(model: Model, services: ModelModelingLanguageServices): string {
-    const referenceStorage = new MmlReferenceStorage(services);
+    const referenceStorage = new MmlReferenceStorage(services.workspace.AstNodeLocator);
+    const instanceRegistry = new MmlInstanceRegistry();
     const serializedModel = new SerializedModel(model, referenceStorage);
-    return JSON.stringify(serializedModel);
+    const serializedInstances = new SerializedInstances(model, referenceStorage, instanceRegistry);
+    return JSON.stringify({typegraph: serializedModel, instancegraph: serializedInstances}, jsonReplacer);
 }
--- test/serialization.test.ts
+++ test/serialization.test.ts
@@ -1,14 +1,88 @@
 import {describe, test} from "vitest";
 import {getSerialization} from "./testutils";
 
-describe('ArithExpr validator tests', () => {
+describe('Serializer tests', () => {
     test('test', async () => {
         const serialization = await getSerialization(`
         package A {
-            enum T {
-                A = 1,
-                B = 2,
-                C = 3
+            class B {
+                attribute int x;
+                reference A.C z;
+            }
+            
+            class C {
+            }
+        }
+        
+        macro T1[] {
+            A.C works {
+            }
+            A.B test{
+                x = 5
+                z -> works
+            }
+        }
+        
+        macro T2[A.B x,A.C y] {
+            x {
+                x = 42
+                z -> y
+            }
+        }
+        
+        macro T3[]{
+            A.C reffing {
+            }
+        }
+        
+        instance gen {
+            tuple x = T1[]
+            A.C newref = T3[].reffing
+            T2[x.test,newref]
+        }
+        `);
+
+        console.warn(serialization);
+    });
+
+    test('test with loop', async () => {
+        const serialization = await getSerialization(`
+        package A {
+            class B {
+                attribute int x;
+                reference A.C z;
+            }
+            
+            class C {
+                attribute int x = 0;
+            }
+        }
+        
+        macro T1[] {
+            A.C w1 {
+            }
+            A.C w2 {
+            }
+            A.C w3 {
+            }
+            A.B test{
+                x = 5
+                z -> w1
+                z -> w2
+                z -> w3
+            }
+        }
+        
+        macro T2[A.C x]{
+            x {
+                x = 42
+            }
+        }
+        
+        instance gen {
+            A.B test = T1[].test
+            for test-z->child {
+                T2[child]
             }
         }
         `);
