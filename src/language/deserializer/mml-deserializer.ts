8b7b91ea362d0badd8f042e4e5b4b0bcae226694
JanikNex <18660622+JanikNex@users.noreply.github.com>
2023-12-28 21:37:15 +0100

    implemented id resolver

--- src/language/deserializer/mml-deserializer.ts
+++ src/language/deserializer/mml-deserializer.ts
@@ -2,8 +2,24 @@ import {SerializedModel} from "../serializer/mml-entity-templates.js";
 import {SerializedInstances} from "../serializer/mml-instance-templates.js";
 import {deserializeModel} from "./mml-entity-deserializer.js";
 import {toString} from "langium";
+import {SerializedDocument} from "../../shared/MmlConnectorTypes.js";
+import {Uri} from "vscode";
+import {MmlIdStorage} from "./mml-id-storage.js";
 
-export function deserializeStringToMMLCode(serialized: string): string {
+export function deserializeStringToMMLCode(serialized: string, idStorage: MmlIdStorage): string {
     const {typegraph}: { typegraph: SerializedModel, instancegraph: SerializedInstances } = JSON.parse(serialized)
-    return toString(deserializeModel(typegraph));
+    return toString(deserializeModel(typegraph, idStorage));
+}
+
+export function deserializeSerializedCLIDoc(cliDoc: string): { modelName: string, modelCode: string } {
+    const sDocs: SerializedDocument[] = JSON.parse(cliDoc);
+    const idStorage: MmlIdStorage = new MmlIdStorage(sDocs);
+    if (sDocs == undefined || sDocs.length != 1) {
+        throw new Error("Unexpected number of serialized documents received!");
+    }
+    const sDoc: SerializedDocument = sDocs.at(0)!;
+    const uniformUri: string = Uri.parse(sDoc.uri).path;
+    const modelName: string = uniformUri.substring(uniformUri.lastIndexOf("/") + 1).replace(".ecore", "").replace(".mml", "");
+    const modelCode: string = deserializeStringToMMLCode(sDoc.content, idStorage);
+    return {modelName: modelName, modelCode: modelCode};
 }
--- src/language/deserializer/mml-entity-deserializer.ts
+++ src/language/deserializer/mml-entity-deserializer.ts
@@ -10,6 +10,7 @@ import {
     ReferenceEntity,
     SerializedModel
 } from "../serializer/mml-entity-templates.js";
+import {MmlIdStorage} from "./mml-id-storage.js";
 
 function joinWithExtraNL<T>(content: T[], toString: (e: T) => Generated): Generated {
     return join(content, toString, {appendNewLineIfNotEmpty: true});
@@ -22,37 +23,37 @@ function setSpacedKeywordIf(keyword: string, condition: boolean): Generated {
     return toNode``;
 }
 
-export function deserializeModel(model: SerializedModel): Generated {
-    return toNode`${joinWithExtraNL(model.packages, pkg => deserializePackage(pkg))}`
+export function deserializeModel(model: SerializedModel, idStorage: MmlIdStorage): Generated {
+    return toNode`${joinWithExtraNL(model.packages, pkg => deserializePackage(pkg, idStorage))}`
 }
 
-function deserializePackage(packageEntity: PackageEntity): Generated {
+function deserializePackage(packageEntity: PackageEntity, idStorage: MmlIdStorage): Generated {
     return toNode`
         package ${packageEntity.name} {
             ${joinWithExtraNL(packageEntity.enums, x => deserializeEnum(x))}
-            ${joinWithExtraNL(packageEntity.abstractClasses, x => deserializeAbstractClassEntity(x))}
-            ${joinWithExtraNL(packageEntity.subPackages, x => deserializePackage(x))}
+            ${joinWithExtraNL(packageEntity.abstractClasses, x => deserializeAbstractClassEntity(x, idStorage))}
+            ${joinWithExtraNL(packageEntity.subPackages, x => deserializePackage(x, idStorage))}
         }
         `
 }
 
-function deserializeAbstractClassEntity(ace: AbstractClassEntity): Generated {
+function deserializeAbstractClassEntity(ace: AbstractClassEntity, idStorage: MmlIdStorage): Generated {
     return toNode`
-        ${setSpacedKeywordIf("abstract", ace.isAbstract)}${setSpacedKeywordIf("interface", ace.isInterface)}class ${ace.name} ${setSpacedKeywordIf("extends " + ace.extendsIds.join(","), ace.extendsIds.length > 0)}${setSpacedKeywordIf("implements " + ace.implementsIds.join(","), ace.implementsIds.length > 0)}{
-            ${joinWithExtraNL(ace.attributes, x => deserializeAttribute(x))}
-            ${joinWithExtraNL(ace.references, x => deserializeReference(x))}
+        ${setSpacedKeywordIf("abstract", ace.isAbstract)}${setSpacedKeywordIf("interface", ace.isInterface)}class ${ace.name} ${setSpacedKeywordIf("extends " + ace.extendsIds.map(x => idStorage.resolveId(x)).join(","), ace.extendsIds.length > 0)}${setSpacedKeywordIf("implements " + ace.implementsIds.map(x => idStorage.resolveId(x)).join(","), ace.implementsIds.length > 0)}{
+            ${joinWithExtraNL(ace.attributes, x => deserializeAttribute(x, idStorage))}
+            ${joinWithExtraNL(ace.references, x => deserializeReference(x, idStorage))}
         }
         `
 }
 
-function deserializeAttribute(attribute: AttributeEntity): Generated {
+function deserializeAttribute(attribute: AttributeEntity, idStorage: MmlIdStorage): Generated {
     if (attribute.hasDefaultValue) {
         return toNode`
-        attribute ${attribute.type} ${attribute.name} = ${attribute.defaultValue}${deserializeClassElementModifiers(attribute.modifiers)};
+        attribute ${attribute.isEnumType ? idStorage.resolveId(attribute.type) : attribute.type} ${attribute.name} = ${attribute.isEnumType ? idStorage.resolveId(attribute.defaultValue as string) : attribute.defaultValue}${deserializeClassElementModifiers(attribute.modifiers)};
         `;
     } else {
         return toNode`
-        attribute ${attribute.type} ${attribute.name}${deserializeClassElementModifiers(attribute.modifiers)};
+        attribute ${attribute.isEnumType ? idStorage.resolveId(attribute.type) : attribute.type} ${attribute.name}${deserializeClassElementModifiers(attribute.modifiers)};
         `;
     }
 }
@@ -62,13 +63,13 @@ function deserializeClassElementModifiers(cem: ClassElementModifiers): Generated
     return modifiers.length == 0 ? toNode`` : toNode` {${modifiers.join(" ")}}`;
 }
 
-function deserializeReference(reference: ReferenceEntity): Generated {
+function deserializeReference(reference: ReferenceEntity, idStorage: MmlIdStorage): Generated {
     if (reference.hasOpposite) {
         return toNode`
-        @opposite ${reference.opposite}
-        reference ${reference.type}${deserializeMultiplicity(reference.multiplicity)} ${reference.name}${deserializeClassElementModifiers(reference.modifiers)};`;
+        @opposite ${idStorage.resolveId(reference.opposite)}
+        reference ${idStorage.resolveId(reference.type)}${deserializeMultiplicity(reference.multiplicity)} ${reference.name}${deserializeClassElementModifiers(reference.modifiers)};`;
     } else {
-        return toNode`reference ${reference.type}${deserializeMultiplicity(reference.multiplicity)} ${reference.name}${deserializeClassElementModifiers(reference.modifiers)};`;
+        return toNode`reference ${idStorage.resolveId(reference.type)}${deserializeMultiplicity(reference.multiplicity)} ${reference.name}${deserializeClassElementModifiers(reference.modifiers)};`;
     }
 }
 
