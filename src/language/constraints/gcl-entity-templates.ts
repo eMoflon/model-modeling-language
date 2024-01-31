fcb30a68c69580a484d56b1cd579562bde4f313c
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-01-31 19:11:14 +0100

    added expression serializer and added attribute constraint entities

--- src/language/constraints/gcl-entity-templates.ts
+++ src/language/constraints/gcl-entity-templates.ts
@@ -1,18 +1,27 @@
 import {
     AbstractElement,
+    Attribute,
+    BinaryExpression,
     CompactBindingStatement,
     ConstraintDocument,
     EnforceAnnotation,
+    EnumEntry,
+    Expression,
     ForbidAnnotation,
+    isBinaryExpression,
     isEnforceAnnotation,
     isForbidAnnotation,
+    isPattern,
     isPatternObject,
+    isValueExpr,
     Pattern,
+    PatternAttributeConstraint,
     PatternObject,
     PatternObjectReference
 } from "../generated/ast.js";
 import {GclReferenceStorage} from "./gcl-reference-storage.js";
 import {ModelModelingLanguageUtils} from "../model-modeling-language-utils.js";
+import {ExprUtils} from "../expr-utils.js";
 
 export class PatternEntity {
     readonly name: string;
@@ -34,6 +43,7 @@ export class PatternEntity {
             }
         })
         this.nodes = pattern.objs.map(x => new PatternNodeEntity(x, (x) => this.registerEdge(x), resolver));
+        this.constraints = pattern.constraints.map(x => new AttributeConstraintEntity(x, resolver));
     }
 
     public registerEdge(edge: EdgeEntity) {
@@ -110,7 +120,80 @@ export class EdgeEntity {
 }
 
 export class AttributeConstraintEntity {
+    readonly isBinary: boolean;
+    readonly expr: PrimaryExpressionEntity | BinaryExpressionEntity;
 
+
+    constructor(ac: PatternAttributeConstraint, resolver: GclReferenceStorage) {
+        this.expr = BinaryExpressionEntity.generateChild(ac.expr, resolver);
+        this.isBinary = isBinaryExpression(ac.expr);
+    }
+}
+
+export class BinaryExpressionEntity {
+    readonly left: PrimaryExpressionEntity | BinaryExpressionEntity;
+    readonly leftIsBinary: boolean;
+    readonly right: PrimaryExpressionEntity | BinaryExpressionEntity;
+    readonly rightIsBinary: boolean;
+    readonly operator: string;
+
+
+    constructor(bexpr: BinaryExpression, resolver: GclReferenceStorage) {
+        this.operator = bexpr.operator;
+        this.left = BinaryExpressionEntity.generateChild(bexpr.left, resolver);
+        this.leftIsBinary = isBinaryExpression(bexpr.left);
+        this.right = BinaryExpressionEntity.generateChild(bexpr.right, resolver);
+        this.rightIsBinary = isBinaryExpression(bexpr.right);
+    }
+
+    public static generateChild(expr: Expression, resolver: GclReferenceStorage): PrimaryExpressionEntity | BinaryExpressionEntity {
+        if (isBinaryExpression(expr)) {
+            return new BinaryExpressionEntity(expr, resolver);
+        }
+        if (isValueExpr(expr)) {
+            return new PrimaryExpressionEntity(expr.value, resolver);
+        }
+        if (ExprUtils.isEnumValueExpression(expr) && expr.val.ref != undefined) {
+            const enumLit: EnumEntry = expr.val.ref;
+            return new PrimaryExpressionEntity(enumLit.name, resolver, "", "", "", false, true);
+        }
+        if (ExprUtils.isAttributeInvocationVariableExpr(expr) && expr.val.ref != undefined && isPattern(ExprUtils.getExprContainer(expr).$container)) {
+            const attr: Attribute = expr.val.ref as Attribute
+            const patternConstraint: PatternAttributeConstraint = ExprUtils.getExprContainer(expr) as PatternAttributeConstraint;
+            const separatedAttributeAccess: string[] = expr.val.$refText.split(".");
+            if (separatedAttributeAccess.length != 2) {
+                throw new Error(`Broke AttributeInvocation "${expr.val.$refText}" but received ${separatedAttributeAccess.length} parts!`)
+            }
+            const patternObjName: string = separatedAttributeAccess.at(0) as string;
+            const node: PatternObject | undefined = patternConstraint.$container.objs.filter(x => x.var.name == patternObjName).find(x => x != undefined);
+            if (node == undefined) {
+                throw new Error(`Unknown pattern node "${patternObjName}"`);
+            }
+            return new PrimaryExpressionEntity("", resolver, ModelModelingLanguageUtils.getQualifiedClassName(attr.$container, attr.$container.name), attr.name, resolver.getNodeReferenceId(node), true, false);
+        }
+        throw new Error(`Missing serializer in BinaryExpressionEntity.generateChild() -> ${expr.$type}`);
+    }
+}
+
+export class PrimaryExpressionEntity {
+    readonly value: number | boolean | string;
+    readonly valueType: string;
+    readonly className: string;
+    readonly elementName: string;
+    readonly nodeId: string;
+    readonly isAttribute: boolean;
+    readonly isEnumLiteral: boolean;
+
+
+    constructor(value: number | boolean | string, resolver: GclReferenceStorage, containerFQName: string = "", elementName: string = "", nodeId: string = "", isAttribute: boolean = false, isEnumLiteral: boolean = false) {
+        this.value = value;
+        this.valueType = typeof value;
+        this.className = containerFQName;
+        this.elementName = elementName;
+        this.nodeId = nodeId;
+        this.isAttribute = isAttribute;
+        this.isEnumLiteral = isEnumLiteral;
+    }
 }
 
 export class ConstraintDocumentEntity {
