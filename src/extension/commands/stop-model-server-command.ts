d513f31866878a63495f1fd3ce7ad439082b3b03
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-03-10 16:43:03 +0100

    fixed model server termination temporarily

--- src/extension/commands/stop-model-server-command.ts
+++ src/extension/commands/stop-model-server-command.ts
@@ -1,23 +1,27 @@
 import {ExtensionCommand} from "./command-utils.js";
 import {LanguageClient} from "vscode-languageclient/node.js";
 import vscode from "vscode";
-import {showUIMessage} from "../../shared/NotificationUtil.js";
-import {MessageType} from "../../shared/MmlNotificationTypes.js";
 import {ModelServerStarter} from "../model-server-starter.js";
+import {ModelServerConnector} from "../model-server-connector.js";
 
 export class StopModelServerCommand extends ExtensionCommand {
     readonly modelServerStarter: ModelServerStarter;
+    readonly modelServerConnector: ModelServerConnector;
 
-    constructor(client: LanguageClient, logger: vscode.OutputChannel, modelServerStarter: ModelServerStarter) {
+    constructor(client: LanguageClient, logger: vscode.OutputChannel, modelServerStarter: ModelServerStarter, modelServerConnector: ModelServerConnector) {
         super("model-modeling-language.stopModelServer", client, logger);
         this.modelServerStarter = modelServerStarter;
+        this.modelServerConnector = modelServerConnector;
     }
 
     execute(...args: any[]): any {
-        if (this.modelServerStarter.terminate()) {
+        //TODO: Fix process termination -> should be able to terminate process in general, not just model server!
+        this.modelServerConnector.clients;
+        this.modelServerConnector.terminate();
+        /*if (this.modelServerStarter.terminate()) {
             showUIMessage(MessageType.INFO, "Terminated ModelServer successfully!")
         } else {
             showUIMessage(MessageType.ERROR, "Could not terminate ModelServer!")
-        }
+        }*/
     }
 }
