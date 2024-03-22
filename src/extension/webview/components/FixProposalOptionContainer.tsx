64191daa72017e8d8da9d65ecfddebce03ff1858
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-03-22 22:41:34 +0100

    added contexts to pass contextual states on variant selection

--- src/extension/webview/components/FixProposalOptionContainer.tsx
+++ src/extension/webview/components/FixProposalOptionContainer.tsx
@@ -8,11 +8,17 @@ import "./FixProposalOptionContainer.css"
 import {FixProposalOption} from "./FixProposalOption.js";
 
 
-export function FixProposalOptionContainer(props: { fixProposalContainer: FixProposalContainer; }) {
-    let {fixProposalContainer} = props;
+export function FixProposalOptionContainer(props: {
+    fixProposalContainer: FixProposalContainer;
+    notifyFixedContainerOption: Function;
+}) {
+    let {fixProposalContainer, notifyFixedContainerOption} = props;
 
     const [proposalContainerExpanded, setProposalContainerExpanded] = React.useState(false);
     const [foldIcon, setFoldIcon] = React.useState("codicon codicon-chevron-right");
+    const [fixHandled, setFixHandled] = React.useState(false);
+    const [fixedProposals, setFixedProposals] = React.useState(0);
+    const [fixedContainers, setFixedContainers] = React.useState(0);
 
     const toggleExpand = () => {
         if (proposalContainerExpanded) {
@@ -22,6 +28,39 @@ export function FixProposalOptionContainer(props: { fixProposalContainer: FixPro
             setProposalContainerExpanded(true);
             setFoldIcon("codicon codicon-chevron-down");
         }
+    }
+
+    const containerFulfilled = (fProposals: number, fContainers: number) => {
+        if (fixProposalContainer.type == FixProposalContainerType.SINGLE_FIX) {
+            return fProposals == fixProposalContainer.proposals.length;
+        } else if (fixProposalContainer.type == FixProposalContainerType.FIX_ONE) {
+            return fProposals >= 1 || fContainers >= 1;
+        } else if (fixProposalContainer.type == FixProposalContainerType.FIX_ALL) {
+            return fProposals == fixProposalContainer.proposals.length && fContainers == fixProposalContainer.proposalContainers.length;
+        }
+        return false;
+    }
+
+    const incrementFixedProposals = () => {
+        setFixedProposals(fixedProposals + 1);
+        if (!fixHandled && containerFulfilled(fixedProposals + 1, fixedContainers)) {
+            handleContainerFulfillment();
+        }
+    }
+
+    const incrementFixedContainers = () => {
+        setFixedContainers(fixedContainers + 1);
+        if (!fixHandled && containerFulfilled(fixedProposals, fixedContainers + 1)) {
+            handleContainerFulfillment();
+        }
+    }
+
+    const handleContainerFulfillment = () => {
+        notifyFixedContainerOption();
+        if (proposalContainerExpanded) {
+            toggleExpand();
+        }
+        setFixHandled(true);
     }
 
     const computedStyle: CSSStyleDeclaration = getComputedStyle(document.documentElement);
@@ -42,17 +81,19 @@ export function FixProposalOptionContainer(props: { fixProposalContainer: FixPro
     }
 
     const containerProposalContainers = fixProposalContainer.proposalContainers.map((propContainer, idx) =>
-        <FixProposalOptionContainer fixProposalContainer={propContainer} key={`container-${idx}`}/>)
+        <FixProposalOptionContainer fixProposalContainer={propContainer} key={`container-${idx}`}
+                                    notifyFixedContainerOption={incrementFixedContainers}/>)
 
     const containerProposals = fixProposalContainer.proposals.map((propsl, idx) => <FixProposalOption proposal={propsl}
-                                                                                                      key={`proposal-${idx}`}/>)
+                                                                                                      key={`proposal-${idx}`}
+                                                                                                      notifyFixedProposalOption={incrementFixedProposals}/>)
 
     return (
         <>
             <div className="ms-fix-proposal-opt-container-wrapper">
                 <div className="ms-fix-proposal-opt-container-header">
                     <div className="ms-fix-proposal-opt-container-header-button-wrapper">
-                        <VSCodeButton appearance="icon" onClick={toggleExpand}>
+                        <VSCodeButton appearance="icon" onClick={toggleExpand} disabled={fixHandled}>
                             <i className={foldIcon} style={{color: iconColor}}></i>
                         </VSCodeButton>
                     </div>
