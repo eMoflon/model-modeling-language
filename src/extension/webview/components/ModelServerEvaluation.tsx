e2d41bd089c93b8251bb013597c45db9c0ee7187
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-03-27 14:49:26 +0100

    prevent multiple repeated rerenders due to duplicate EventListeners

--- src/extension/webview/components/ModelServerEvaluation.tsx
+++ src/extension/webview/components/ModelServerEvaluation.tsx
@@ -21,51 +21,51 @@ export function ModelServerEvaluationWrapper() {
 function ModelServerEvaluation() {
     const evalContext: ModelServerEvaluationCtxt = useModelServerEvaluationContext();
 
+    const handleMessage = (event: MessageEvent) => {
+        const message = event.data; // The json data that the extension sent
+        switch (message.command) {
+            case 'updateView':
+                console.log("[ModelServerEvaluation] Received updateView");
+                if (message.success) {
+                    console.log("[ModelServerEvaluation] Request was successful");
+                    const deserializedMessage: GetConstraintsResponse = GetConstraintsResponse.fromJsonString(message.data);
+
+                    evalContext.setConstraints(deserializedMessage.constraints);
+                    const constraints: Constraint[] = deserializedMessage.constraints;
+                    //setDebugText(JSON.stringify(constraints));
+                    console.log(message.data);
+
+                    evalContext.setTotalConstraints(constraints.length);
+                    evalContext.setViolatedConstraints(constraints.filter(x => x.violated).length);
+
+                    evalContext.setLoadState("loaded");
+                } else {
+                    console.log("[ModelServerEvaluation] Request was NOT successful");
+                    evalContext.setConstraints([]);
+                    //setDebugText(`Could not reach ModelServer!\n(Reason: ${message.data})`);
+                    console.log(message.data);
+
+                    evalContext.setTotalConstraints(0);
+                    evalContext.setViolatedConstraints(0);
+
+                    evalContext.setLoadState("error");
+                }
+                break;
+            case 'modelRepairPerformed':
+                if (message.success) {
+                    evalContext.requestConstraintEvaluation();
+                } else {
+                    evalContext.setConstraints([]);
+                    evalContext.setTotalConstraints(0);
+                    evalContext.setViolatedConstraints(0);
+                    evalContext.setLoadState("error");
+                }
+        }
+    }
+
     React.useEffect(() => {
-        window.addEventListener('message', event => {
-            const message = event.data; // The json data that the extension sent
-            switch (message.command) {
-                case 'updateView':
-                    console.log("[ModelServerEvaluation] Received updateView");
-                    if (message.success) {
-                        console.log("[ModelServerEvaluation] Request was successful");
-                        const deserializedMessage: GetConstraintsResponse = GetConstraintsResponse.fromJsonString(message.data);
-
-                        evalContext.setConstraints(deserializedMessage.constraints);
-                        const constraints: Constraint[] = deserializedMessage.constraints;
-                        //setDebugText(JSON.stringify(constraints));
-                        console.log(message.data);
-
-                        evalContext.incrementEvaluationCount();
-
-                        evalContext.setTotalConstraints(constraints.length);
-                        evalContext.setViolatedConstraints(constraints.filter(x => x.violated).length);
-
-                        evalContext.setLoadState("loaded");
-                    } else {
-                        console.log("[ModelServerEvaluation] Request was NOT successful");
-                        evalContext.setConstraints([]);
-                        //setDebugText(`Could not reach ModelServer!\n(Reason: ${message.data})`);
-                        console.log(message.data);
-
-                        evalContext.setTotalConstraints(0);
-                        evalContext.setViolatedConstraints(0);
-
-                        evalContext.setLoadState("error");
-                    }
-                    break;
-                case 'modelRepairPerformed':
-                    if (message.success) {
-                        evalContext.requestConstraintEvaluation();
-                    } else {
-                        evalContext.setConstraints([]);
-                        evalContext.setTotalConstraints(0);
-                        evalContext.setViolatedConstraints(0);
-                        evalContext.setLoadState("error");
-                    }
-            }
-        });
-    },);
+        window.addEventListener('message', handleMessage, false);
+    }, []);
 
     return (
         <div>
